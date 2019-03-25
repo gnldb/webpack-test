db7b5ddbc02c2bb8cf6b286181218351a137f1ae
gnldb <gnldb1117@gmail.com>
2019-03-25 16:52:09 +0900

    another commmit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+import  './style.css';
+import  './hello.scss';
+
+function component() {
+	let element = document.createElement('div');
+
+	// Lodash, currently included via a script, is required for this line to work
+	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+	return element;
+}
+
+document.body.appendChild(component());
+
+console.log('hello webpack dev server');
